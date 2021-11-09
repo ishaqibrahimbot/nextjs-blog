79c4e5ba8392105314ff2497dd9a595b66c06a49
Ishaq Ibrahim <ishaqibrahimbss@gmail.com>
2021-11-09 14:21:03 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className="container mx-auto px-56 flex flex-col items-center mt-12">
+        <p className="text-xl">
+          Hello, I'm Ishaq. I'm a software engineer at Teamo. I live in Karachi,
+          Pakistan.
+        </p>
+        <p className="text-xl mt-5">
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a className="text-blue-600" href="https://nextjs.org/learn">
+            our Next.js tutorial
+          </a>
+          .)
+        </p>
+      </section>
+      <section className="container flex flex-col items-start px-56 mt-8">
+        <h2 className="text-2xl font-bold">Blog</h2>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="mt-4" key={id}>
+              <Link href={`/posts/${id}`}>
+                <a className="text-xl text-blue-600">{title}</a>
+              </Link>
+              <br />
+              <small className="text-lg text-gray-500">
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <div className="px-44 mt-6">
+        <div className="text-3xl font-black">
+          <h1>{postData.title}</h1>
+        </div>
+        <br />
+        <div className="text-gray-400 font-semibold">
+          <Date dateString={postData.date} />
+        </div>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </div>
+    </Layout>
+  );
+}
